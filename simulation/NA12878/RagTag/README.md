effc054c0d5d8e70c1bb25181c36d6a042a7f6b8
Adam Diehl <adadiehl@umich.edu>
2025-02-05 11:29:00 -0500

    Add NA12878 RagTag analysis

---
+++ simulation/NA12878/RagTag/README.md
@@ -0,0 +1,25 @@
+# Simulated Contig Genome Patching with RagTag Patch
+## T2T-CHM13 pseudocontigs based on HGSVC NA12878 contig size distribution
+## The following steps were used for patching simulated CHM13 contigs using RagTag Patch:
+
+## Dependencies
+* Python >= v3.7
+* RagTag (https://github.com/malonge/RagTag/)
+* samtools (https://github.com/samtools/samtools)
+* biopython (https://biopython.org/)
+* pysam (https://github.com/pysam-developers/pysam)
+* minimap2 (https://github.com/lh3/minimap2)
+
+### RagTag patching of NA12878 simulated contigs, no indels
+```
+/usr/bin/time -v ./do_RagTag_Patch.sh ../CHM13.pseudocontigs.NA12878.1.rc.contigs.fa ../../../data/GCA_009914755.4_T2T-CHM13v2.0_genomic.chroms.fa T2T-CHM13 CHM13.NA12878.1 no_indel
+../../compare_contig_positions.py -r ../CHM13.pseudocontigs.NA12878.1.rc.contigs.bed -p no_indel/ragtag.patch.contigs.bed > no_indel/ragtag.patch.contigs.mapping-stats.txt
+../../compare_contig_ordering.py -r ../CHM13.pseudocontigs.NA12878.1.rc.contigs.bed -p no_indel/ragtag.patch.contigs.bed > no_indel/ragtag.patch.contigs.ordering-stats.txt
+../../compare_fasta_by_editdist.py -r ../../../data/GCA_009914755.4_T2T-CHM13v2.0_genomic.chroms.fa -p no_indel/ragtag.patch.scf.fasta > no_indel/ragtag.patch.contigs.editdist-stats.txt
+../../compile_mapping_stats.Rscript no_indel/ragtag.patch.contigs.mapping-stats.txt > no_indel/ragtag.patch.contigs.mapping-summary.txt
+```
+
+### RagTag patching of NA12878 simulated contigs, with simulated indels from SURVIVOR
+```
+/usr/bin/time -v ./do_RagTag_Patch.SURVIVOR.sh ../CHM13.pseudocontigs.NA12878.1.rc.contigs.SURVIVOR.with-missing.fasta ../../../data/GCA_009914755.4_T2T-CHM13v2.0_genomic.chroms.fa T2T-CHM13.SURVIVOR CHM13.NA12878.1.SURVIVOR SURVIVOR
+```
---
+++ simulation/NA12878/RagTag/do_RagTag_Patch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Run RagTag Patch using the given reference and query genomes and
+# convert results to follow GPatch conventions.
+
+ASSEMBLY_FASTA=$1
+REFERENCE_FASTA=$2
+REFERENCE_NAME=$3
+PREFIX=$4
+OUT_DIR=$5
+
+SCRIPTS_PATH=/data/projects/adadiehl/genome_patching/GPatch/scripts
+
+# Run RagTag Patch
+/usr/bin/time -v ragtag.py patch --aligner minimap2 --mm2-params "-x asm20 -c -t 24" -o $OUT_DIR -f 100 -s 10000 $ASSEMBLY_FASTA $REFERENCE_FASTA
+
+# Convert AGP output to contigs.bed
+../../RagTag_agp_to_contigs_bed.py -r $OUT_DIR/ragtag.patch.rename.agp -c $OUT_DIR/ragtag.patch.ctg.agp -p $OUT_DIR/ragtag.patch.agp > $OUT_DIR/ragtag.patch.contigs.bed
+
+# Filter out only scaffolds from FASTA output
+../../process_ragtag_fasta.py -f $OUT_DIR/ragtag.patch.fasta -r $OUT_DIR/ragtag.patch.rename.agp -p $OUT_DIR/ragtag.patch.agp -c $OUT_DIR/ragtag.patch.ctg.agp > $OUT_DIR/ragtag.patch.scf.fasta
+
+# Pull out some quick assembly stats
+printf "pre-break contig base count: %d\n" $(awk 'BEGIN {len=0} {len += ($3-$2)} END {print len}' $OUT_DIR/ragtag.patch.contigs.bed) > $OUT_DIR/ragtag.patch.stats
+#printf "pre-break patch base count: %d\n" $(awk 'BEGIN {len=0} {len += ($3-$2)} END {print len}' $OUT_DIR/$PREFIX.patches.bed) >> $OUT_DIR/ragtag.patch.stats
+printf "Initial unpatched assembly genome size (bp): %d\n" $(grep -v ">" $ASSEMBLY_FASTA | awk 'BEGIN{N=0}{N+=length($0)}END{print N}') >> $OUT_DIR/ragtag.patch.stats
+printf "pre-break total patched genome size (bp): %d\n" $(grep -v ">" $OUT_DIR/ragtag.patch.scf.fasta | awk 'BEGIN{N=0}{N+=length($0)}END{print N}') >> $OUT_DIR/ragtag.patch.stats
+
+# Alignment to reference and dot-plots.
+minimap2 -x asm20 -t 24 $REFERENCE_FASTA $OUT_DIR/ragtag.patch.scf.fasta > $OUT_DIR/ragtag.patch.$REFERENCE_NAME.paf
+$SCRIPTS_PATH/create_dotplots.Rscript $OUT_DIR/ragtag.patch.$REFERENCE_NAME.paf $PREFIX $REFERENCE_NAME
+mv *.pdf $OUT_DIR
